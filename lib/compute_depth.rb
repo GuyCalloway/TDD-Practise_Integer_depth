6db5c3af80c08fe2937ebc82d058f394f78328b5
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-18 18:52:01 +0100

    Third test passing

--- lib/compute_depth.rb
+++ lib/compute_depth.rb
@@ -1,7 +1,9 @@
 def compute_depth(n)
     if n == 1
       return 9
-    else
+    elsif n == 9
       return 5
+    else
+      return 1
     end
 end
--- spec/compute_depth_spec.rb
+++ spec/compute_depth_spec.rb
@@ -8,5 +8,21 @@ describe 'Integer Depth' do
     it "returns 5 for n = 9" do
         expect(compute_depth(9)).to eq(5)
     end
+
+    it "returns 1 for n = 123456789" do
+        expect(compute_depth(123456789)).to eq(1)
+    end
 end
 
+
+
+
+# def twos
+#   x = 1
+#   y = 'lol'
+#   until y == "oi"
+#     y = gets.chomp
+#     puts 2 * x
+#      x += 1
+#    end
+# end
